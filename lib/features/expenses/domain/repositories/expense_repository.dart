db7248e10020ae57a6bc1d25992573648cae472a
ravi bhatt <ravi2018bhatt@gmail.com>
2026-06-29 12:31:59 +0545

    feat: add Failure classes and update repository interface with Either

--- lib/features/expenses/domain/repositories/expense_repository.dart
+++ lib/features/expenses/domain/repositories/expense_repository.dart
@@ -1,13 +1,16 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
 import '../entities/expense.dart';
 
 abstract class ExpenseRepository {
-  Future<void> addExpense(Expense expense);
-  Future<List<Expense>> getExpense({
+  Future<Either<Failure, void>> addExpense(Expense expense);
+  Future<Either<Failure, List<Expense>>> getExpense({
     TransactionType? type,
     String? categoryId,
     DateTime? startDate,
     DateTime? endDate,
   });
-  Future<void> updateExpense(Expense expense);
-  Future<void> deleteExpense(String id);
+  Future<Either<Failure, void>> updateExpense(Expense expense);
+  Future<Either<Failure, void>> deleteExpense(String id);
 }
